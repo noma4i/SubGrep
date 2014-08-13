1bc5ea94307cdccec4f8f76ec545d05ab2f52a38
noma4i <noma4i@gmail.com>
2014-08-13 11:40:58 +1000

    Mockup

---
+++ SubGrep.py
@@ -0,0 +1,23 @@
+import sublime, sublime_plugin
+
+class SubGrepCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    title = "Grep: [TEST]"
+    selection = "TEST"
+    self.view.run_command( 'show_grep', {'title': title, 'selection': selection} )
+
+class ShowGrepCommand(sublime_plugin.TextCommand):
+  def run(self,edit,title,selection):
+    self.grep_view(edit,title,selection)
+
+  def grep_view(self, edit, title, content, **kwargs):
+    syntax = 'Packages/Markdown/Markdown.tmLanguage'
+    panel = self.view.window().new_file()
+    panel.set_name(title)
+    panel.set_scratch(True)
+    panel.set_syntax_file(syntax)
+
+    panel.insert(edit, 0, content)
+    panel.set_read_only(True)
+
+    return panel
